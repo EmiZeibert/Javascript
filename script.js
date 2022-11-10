7bfa5b58d7e73e7b3d6c8758764939cfd9e123a0
Emilce Zeibert <emilcezeibert@hotmail.com>
2022-11-10 19:11:24 -0300

    haciendo segunda entrega

--- script.js
+++ script.js
@@ -1,69 +1,147 @@
-function Saludar() {   
-alert("¡Hola vamos a comenzar!");
-}
-const empresa={
-    nombre:"Coderhouse",
-    cantidadDeEmpleados:"N°"+" "+"empleados"+ " " + 100,
-    calle:"Buenos aires 1234"
-}
-console.log(empresa["nombre"])
-console.log(empresa["cantidadDeEmpleados"])
-console.log(empresa["calle"])
-let nombre = prompt("Ingrese su nombre");
-let apellido = prompt("Ingrese su apellido");
-let resultado = nombre + " " + apellido;
-console.log(resultado)
-let salida = "Bienvenida/o" + " " + nombre + " " + apellido;
-alert(salida);
-let turno;
-do {
-    turno = prompt("Ingrese 1.Turno 9 a 15/2.Turno 15 a 21/3.Turno 18 a 00 (solo numero)");
-} while (turno > 3 && turno)
-switch (turno) {
-    case "1":
-        alert("Sus vacaciones son del 20/10/22 al 05/11/2022")
-        break
-    case "2":
-        alert("Sus vacaciones son del 05/11/22 al 15/11/2022")
-        break
-    case "3":
-        alert("Sus vacaciones son del 15/11/22 al 30/11/2022")
-        break
+function Saludar() {
+    console.log("¡Hola vamos a comenzar!");
 
-    default:
-        alert("opcion ingresada invalida")
-        break
 }
-console.log("¿Hay algo más que te quisiera consultar?");
-let turno2;
-do {
-    turno2 = prompt("Ingrese 1.Cobros/2.Fecha/0.Salir");
-} while (turno2 > 2 && turno2)
-    ;
-if (turno2 == 1) {
-    var hsTrabajo = parseInt(prompt("ingrese cantidad de horas de trabajo(solo numeros)"));
-    var salario = 300 * hsTrabajo;
-    var deduccion = salario * 10 / 100;
-    var monto = salario - deduccion;
-    alert("Monto a cobrar" + " " + monto.toFixed(2));
-    alert("Hasta luego");
-
-} else if (turno2 == 2) {
-    var ultNum = Number(prompt("ingrese su ultimo digito del DNI"));
-    }
-   
-if (ultNum % 2 == 0) {        
-      alert("Numero par" + " " +"fecha de cobro" + " " + 1 + "er" + " " + "dia habil");
+Saludar();
+const empresa = {
+    nombre: "Electronic",
+    calle: "Buenos aires 1234",
+    informacion: "Venta de productos electronicos"
     
 }
-else if (ultNum * 3) {
-    alert("Numero impar" + " " + "fecha de cobro" + " " + 5 + "to" + " " + "dia habil");
-    alert("Hasta luego");
+console.log(empresa["nombre"])
+console.log(empresa["calle"])
+console.log(empresa["informacion"])
+
+let nombre = prompt("Ingrese su nombre completo");
+let resultado = nombre;
+console.log(resultado)
+let salida = "Bienvenida/o" + " " + nombre;
+function Descuento() {
+    console.log("¡Solo por hoy $5000 de descuento!");
 
 }
-else if (turno2 == 0) {
-    alert("Hasta luego");
+Descuento();
+alert(salida);
+
+const productos = [
+    { id: 1, nombre: "notebook", precio: 100000 },
+    { id: 2, nombre: "parlante", precio: 20000 },
+    { id: 3, nombre: "celular", precio: 99000 },
+    { id: 4, nombre: "smartv", precio: 58000 },
+    { id: 5, nombre: "tablet", precio: 28000 },
+    { id: 6, nombre: "monitor 22p", precio: 15000 },
+    
+];
+function Productos(titulo,color){
+    this.titulo=titulo;
+    this.color=color;
+    
 }
+
+productos.forEach((producto) => {
+    console.log(producto.nombre)
+    console.log(producto.precio)
+   
+
+});
+const producto1= new Productos ("notebook","gris")
+const producto2= new Productos ("parlante","negro")
+const producto3= new Productos ("celular","azul")
+const producto4= new Productos ("smartv","negro")
+const producto5= new Productos ("tablet","blanca")
+const producto6= new Productos ("monitor","negro")
+console.log(producto1,producto2,producto3,producto4,producto5,producto6)
+let opcion;
+do {
+opcion = prompt("¿Que producto quiere comprar? \n1.notebook\n2.parlante\n3.celular\n4.smartv\n5.tablet\n6.monitor22p");
+}while (opcion > 6 && opcion)
+;
+switch (opcion) {
+    case "1":
+        console.log("se agrego Notebook x $100000")
+        cantidad = prompt("¿cantidad?");
+        var descuento = 5000
+    var subtotal = (100000  * cantidad) - 5000;
+    console.log(subtotal)
+    alert("Monto a pagar" + " " + subtotal.toFixed(2));
+
+        break;
+        case "2":
+            console.log("Se agrego Parlante x $20000")
+            cantidad = prompt("¿cantidad?");
+            var descuento = 5000
+            var subtotal = (20000 * cantidad) - descuento;
+           
+            console.log(subtotal)
+            alert("Monto a pagar" + " " + subtotal.toFixed(2));
+        break;
+        case "3":
+            console.log("Se agrego Celular x $99000")
+            cantidad = prompt("¿cantidad?");
+            var descuento = 5000
+            var subtotal = (99000 * cantidad) - descuento;
+                     console.log(subtotal)
+            alert("Monto a pagar" + " " + subtotal.toFixed(2));
+        break;
+        case "4":
+            console.log("Se agrego Smartv x $58000 ")
+            cantidad = prompt("¿cantidad?");
+            var descuento = 5000
+            var subtotal = (58000* cantidad)- descuento;
+            console.log(subtotal)
+            alert("Monto a pagar" + " " + subtotal.toFixed(2));
+        break;
+        case "5":
+            console.log("Se agrego Tablet x $28000")
+            cantidad = prompt("¿cantidad?");
+            var descuento = 5000
+            var subtotal = (28000 * cantidad) - descuento;
+                        console.log(subtotal)
+            alert("Monto a pagar" + " " + subtotal.toFixed(2));
+        break;
+        case "6":
+            console.log("Se agrego Monitor x $15000 ")
+            cantidad = prompt("¿cantidad?");
+            var descuento = 5000
+            var subtotal = (15000 * cantidad)- descuento ;
+                    console.log(subtotal)
+            alert("Monto a pagar" + " " + subtotal.toFixed(2));
+        break;
+
+    default: alert("producto inexistente")
+        break;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
 
 
 
