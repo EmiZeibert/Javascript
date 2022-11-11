584e026c149ac563098fd488a1a933547ff6d32a
Emilce Zeibert <emilcezeibert@hotmail.com>
2022-11-11 19:47:41 -0300

    segunda entrega

--- script.js
+++ script.js
@@ -27,6 +27,7 @@ alert(salida);
 const productos = [
     { id: 1, nombre: "notebook", precio: 100000 },
     { id: 2, nombre: "parlante", precio: 20000 },
+    { id: 2, nombre: "parlante", precio: 15000 },
     { id: 3, nombre: "celular", precio: 99000 },
     { id: 4, nombre: "smartv", precio: 58000 },
     { id: 5, nombre: "tablet", precio: 28000 },
@@ -37,12 +38,10 @@ const productos = [
 productos.forEach((producto) => {
     console.log(producto.nombre)
     console.log(producto.precio)
-   
-
-});
-
-
-
+   });
+ let filtro = productos.filter(producto => producto.nombre === "parlante" && producto.precio < 25000)
+ console.log(filtro);
+  
 function Productos(titulo,color){
     this.titulo=titulo;
     this.color=color;
@@ -50,17 +49,13 @@ function Productos(titulo,color){
 }
 const producto1= new Productos ("notebook","gris")
 const producto2= new Productos ("parlante","negro")
-const producto3= new Productos ("celular","azul")
-const producto4= new Productos ("smartv","negro")
-const producto5= new Productos ("tablet","blanca")
-const producto6= new Productos ("monitor","negro")
+const producto3= new Productos ("parlante","azul")
+const producto4= new Productos ("celular","azul")
+const producto5= new Productos ("smartv","negro")
+const producto6= new Productos ("tablet","blanca")
+const producto7= new Productos ("monitor","negro")
 console.log(producto1,producto2,producto3,producto4,producto5,producto6)
-let promociones = productos.filter(producto => producto.nombre === "smartv" && producto.precio < 59000)
-console.log(promociones) 
 
-for (const producto of promociones) {
-  console.log(producto.id, producto.nombre)
-}
 let opcion;
 do {
 opcion = prompt("¿Que producto quiere comprar? \n1.notebook\n2.parlante\n3.celular\n4.smartv\n5.tablet\n6.monitor22p");
@@ -111,18 +106,18 @@ switch (opcion) {
         break;
         case "6":
             console.log("Se agrego Monitor x $15000 ")
-            cantidad = prompt("¿cantidad?");
-            var descuento = 5000
-            var subtotal = (15000 * cantidad)- descuento ;
-                    console.log(subtotal)
-            alert("Monto a pagar" + " " + subtotal.toFixed(2));
+            
         break;
 
     default: alert("producto inexistente")
         break;
 }
 
-
+cantidad = prompt("¿cantidad?");
+var descuento = 5000
+var subtotal = (producto.precio * cantidad)- descuento ;
+        console.log(subtotal)
+alert("Monto a pagar" + " " + subtotal.toFixed(2));
 
 
 
