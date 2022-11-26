4ebb8baaa9781fd1852f62ab177fab718fe510b3
Emilce Zeibert <emilcezeibert@hotmail.com>
2022-11-26 02:04:34 -0300

    Agregando eventos

--- script.js
+++ script.js
@@ -1,159 +1,192 @@
-/*let h1 = document.getElementsByClassName("titulo");
-console.log(h1.innerHTML);
-function Saludar() {
-    console.log("¡Hola vamos a comenzar!");
-
-}
-Saludar();
-const empresa = {
-    nombre: "Electronic",
-    calle: "Buenos aires 1234",
-    informacion: "Venta de productos electronicos"
-
-}
-console.log(empresa["nombre"])
-console.log(empresa["calle"])
-console.log(empresa["informacion"])
-
-
-let nombre = prompt("Ingrese su nombre completo");
-let resultado = nombre;
-console.log(resultado)
-let salida = "Bienvenida/o" + " " + nombre;
-function sumar(descuento1, descuento2){
-    return descuento1 + descuento2
-}
-let result = sumar (2500, 2500);
-
-console.log("¡Solo por hoy"+" "+"$"+ result +" " + "de descuento!")
-const div = document.createElement("div");
 const productos = [
-    { id: 1, nombre: "notebook", precio: 85000 },
-    { id: 2, nombre: "notebook", precio: 65000 },
-    { id: 3, nombre: "parlante", precio: 10500 },
-    { id: 4, nombre: "parlante", precio: 20600 },
-    { id: 5, nombre: "celular samsung A03", precio: 52000 },
-    { id: 6, nombre: "celular motorola E20", precio: 34000 },
-    { id: 7, nombre: "smartv 43", precio: 73000 },
-    { id: 8, nombre: "smartv 32", precio: 45000 }
+    {
+        id: "Celulares-notebook",
+        titulo: "Notebook Exo",
+        precio: 85000,
+        categoria: {
+            nombre: "Celulares-notebook",
+            id: "Celulares-notebook"
+        },
+        imagen: "./imagenes/notebook65000.jpg"
+    },
+    {
+        id: "Celulares-notebook",
+        titulo: "Notebook Acer",
+        precio: 65000,
+        categoria: {
+            nombre: "Celulares-notebook",
+            id: "Celulares-notebook"
+        },
+        imagen: "./imagenes/notebook85000.jpg"
+    },
+    {
+        id: "Audio-video",
+        titulo: "Parlante chico",
+        precio: 10500,
+        categoria: {
+            nombre: "Audio-video",
+            id: "Audio-video"
+        },
+        imagen: "./imagenes/parlante10500.jpg"
+    },
+    {
+        id: "Audio-video",
+        titulo: "Parlante grande",
+        precio: 20600,
+        categoria: {
+            nombre: "Audio-video",
+            id: "Audio-video"
+        },
+        imagen: "./imagenes/parlante20000.jpg"
+    },
+    {
+        id: "Celulares-notebook",
+        titulo: "Celular samsung A03",
+        precio: 52000,
+        categoria: {
+            nombre: "Celulares-notebook",
+            id: "Celulares-notebook"
+        },
+        imagen: "./imagenes/samsunga03.jpg"
+    },
+    {
+        id: "Celulares-notebook",
+        titulo: "Celular motorola E20",
+        precio: 34000,
+        categoria: {
+            nombre: "Celulares-notebook",
+            id: "Celulares-notebook"
+        },
+        imagen: "./imagenes/motorolae20.jpg"
+    },
+    {
+        id: "Audio-video",
+        titulo: "Smartv 43p",
+        precio: 73000,
+        categoria: {
+            nombre: "Audio-video",
+            id: "Audio-video"
+        },
+        imagen: "./imagenes/smart43.jpg"
+    },
+    {
+        id: "Audio-video",
+        titulo: "Smartv 32p",
+        precio: 45000,
+        categoria: {
+            nombre: "Audio-video",
+            id: "Audio-video"
+        },
+        imagen: "./imagenes/smart32.jpg"
+    },
+    {
+        id: "Audio-video",
+        titulo: "Auricular Newvision",
+        precio: 2500,
+        categoria: {
+            nombre: "Audio-video",
+            id: "Audio-video"
+        },
+        imagen: "./imagenes/auricular2500.jpg"
+    },
+    {
+        id: "Audio-video",
+        titulo: "Auricular Redragon",
+        precio: 5000,
+        categoria: {
+            nombre: "Audio-video",
+            id: "Audio-video"
+        },
+        imagen: "./imagenes/auricular5000.jpg"
+    },
+    {
+        id: "Audio-video",
+        titulo: "Tablet",
+        precio: 45000,
+        categoria: {
+            nombre: "Audio-video",
+            id: "Audio-video"
+        },
+        imagen: "./imagenes/tablet45000.jpg"
+    },
+    {
+        id: "Audio-video",
+        titulo: "Tablet Lenovo",
+        precio: 54000,
+        categoria: {
+            nombre: "Audio-video",
+            id: "Audio-video"
+        },
+        imagen: "./imagenes/tablet54000.jpg"
+    }
+
+
 
 ];
+const contenedorProductos = document.querySelector("#contenedor-productos");
+const botonesCategoria = document.querySelectorAll(".boton-categoria");
+let agregarP = document.querySelectorAll(".producto-agregar");
+const tPrincipal = document.querySelector("#tPrincipal");
 
-productos.forEach((producto) => {
-    console.log(producto.nombre)
-    console.log(producto.precio)
-});
-let filtro = productos.filter(producto => producto.nombre === "parlante" && producto.precio < 25000)
-console.log(filtro);
 
-function Productos(titulo, color) {
-    this.titulo = titulo;
-    this.color = color;
+function cargarProductos(elegirProductos) {
+    contenedorProductos.innerHTML = "";
 
+    elegirProductos.forEach(producto => {
+        const div = document.createElement("div");
+        div.classList.add("producto");
+        div.innerHTML = `
+    <img class="producto-imagen" src="${producto.imagen}" alt="${producto.titulo}">
+                    <div class="producto-detalles">
+                        <h3 class="producto-titulo">${producto.titulo}</h3>
+                        <p class="producto-precio">$${producto.precio}</p>
+                        <button class="producto-agregar" id=${producto.id} "">Agregar</button>
+                    </div>
+    `;
+        contenedorProductos.append(div);
+    })
+    actBotones();
 }
-const producto1 = new Productos("notebook", "gris")
-const producto2 = new Productos("parlante", "negro")
-const producto3 = new Productos("parlante", "azul")
-const producto4 = new Productos("celular", "azul")
-const producto5 = new Productos("smartv", "negro")
-const producto6 = new Productos("tablet", "blanca")
-const producto7 = new Productos("monitor", "negro")
-console.log(producto1, producto2, producto3, producto4, producto5, producto6)
-let opcion;
-do {
-    opcion = prompt("¿Que producto quiere comprar? \n1.Notebook\n2.Parlante\n3.Parlante azul\n4.Celular\n5.Smartv\n6.Tablet\n7.Monitor22p");
-} while (opcion > 7 || opcion == 0  && opcion)
-    ;
- 
-switch (opcion) {
-    case "1":
-        console.log("se agrego Notebook x $100000")
-        cantidad = prompt("¿cantidad?");
-        var descuento = 5000
-        var subtotal = (100000 * cantidad) - 5000;
-        console.log(subtotal)
-        alert("Monto a pagar" + " " + subtotal.toFixed(2));
+cargarProductos(productos);
 
-        break;
-    case "2":
-        console.log("Se agrego Parlante x $20000")
-        cantidad = prompt("¿cantidad?");
-        var descuento = 5000
-        var subtotal = (20000 * cantidad) - descuento;
+botonesCategoria.forEach(boton => {
+    boton.addEventListener("click", (e) => {
+        botonesCategoria.forEach(boton => boton.classList.remove("active"));
+        e.currentTarget.classList.add("active");
 
-        console.log(subtotal)
-        alert("Monto a pagar" + " " + subtotal.toFixed(2));
-        break;
-    case "3":
-        console.log("Se agrego Parlante x $15000")
-        cantidad = prompt("¿cantidad?");
-        var descuento = 5000
-        var subtotal = (15000 * cantidad) - descuento;
+        if (e.currentTarget.id != "todos") {
+            const productosFiltrar = productos.filter(producto => producto.categoria.id === e.currentTarget.id);
+            cargarProductos(productosFiltrar);
+            const tituloCategoria = productos.find(producto => producto.categoria.id === e.currentTarget.id);
+            tPrincipal.innerText = tituloCategoria.categoria.nombre;
+        } else {
+            tPrincipal.innerText = "Todos los productos";
+            cargarProductos(productos);
+        }
 
-        console.log(subtotal)
-        alert("Monto a pagar" + " " + subtotal.toFixed(2));
-        break;
-    case "4":
-        console.log("Se agrego Celular x $99000")
-        cantidad = prompt("¿cantidad?");
-        var descuento = 5000
-        var subtotal = (99000 * cantidad) - descuento;
-        console.log(subtotal)
-        alert("Monto a pagar" + " " + subtotal.toFixed(2));
-        break;
-    case "5":
-        console.log("Se agrego Smartv x $58000 ")
-        cantidad = prompt("¿cantidad?");
-        var descuento = 5000
-        var subtotal = (58000 * cantidad) - descuento;
-        console.log(subtotal)
-        alert("Monto a pagar" + " " + subtotal.toFixed(2));
-        break;
-    case "6":
-        console.log("Se agrego Tablet x $28000")
-        cantidad = prompt("¿cantidad?");
-        var descuento = 5000
-        var subtotal = (28000 * cantidad) - descuento;
-        console.log(subtotal)
-        alert("Monto a pagar" + " " + subtotal.toFixed(2));
-        break;
-    case "7":
-        console.log("Se agrego Monitor x $15000 ")
-        cantidad = prompt("¿cantidad?");
-        var descuento = 5000
-        var subtotal = (15000 * cantidad) - descuento;
-        console.log(subtotal)
-        alert("Monto a pagar" + " " + subtotal.toFixed(2));
-        break;
+    })
 
-    default: alert("Producto inexistente")
-        break;
+})
+function actBotones() {
+    agregarP = document.querySelectorAll(".producto-agregar");
+    agregarP.forEach(boton => {
+        boton.addEventListener("click", agregarAlCarrito);
+    });
 }
-
-function Finalizarcompra() {
-    console.log("¡Vamos a finalizar la compra!");
-
+const productoElegidos = [];
+function agregarAlCarrito(e) {
+    const idBoton = e.currentTarget.id;
+    const productoAgregado = productos.find(producto => producto.id === idBoton)
+    if( productoElegidos.some(producto => producto.id === idBoton)){
+        const buscar= productoElegido.findIndex(producto => producto.id === idBoton);
+        productoElegido[buscar].cantidad++
+       
+    }else{
+        productoAgregado.cantidad= 1;
+        productoElegidos.push(productoAgregado);
+    }
+   
 }
-Finalizarcompra();
-var envio=500
-var total= subtotal + envio;
-alert("Su pago total es" + " " + total)
-function Saludar() {
-    console.log("¡Gracias por su compra!");
-
-}
-Saludar();
-
-let indice = opcion.indexOf("7");
-console.log(indice);*/
-
-
-
-
-
-
-
-
 
 
 
